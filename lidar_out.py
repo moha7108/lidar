8c64c3841c241232ec2dd26d11aeb8966c5120db
moha7108 <moha7108@protonmail.com>
2023-07-10 00:52:50 -0400

    update outputs to relevant angles only

--- lidar_out.py
+++ lidar_out.py
@@ -1,16 +1,28 @@
 import json
 from rplidar import RPLidar
 
+RELEVANT_ANGLES = [0,90,180,270]
+
 lidar = RPLidar('/dev/ttyUSB0')
 
-try:
-    print('Recording measurements... Press Ctrl+C to stop.')
-    # Fetch 10 measurements for the example
-    for i, measurement in enumerate(lidar.iter_measurements()):
-        if i > 10:
-            break
-        print(json.dumps(measurement))
-except KeyboardInterrupt:
-    print('Stopping.')
-lidar.stop()
-lidar.disconnect()
+print('Recording measurements... Press Ctrl+C to stop.')
+
+while True:
+    try:
+        # Fetch 10 measurements for the example
+        for scan in lidar.iter_scans():
+            
+            relevant_out={}
+
+            for measurement in scan:
+                quality, angle, distance = measurement
+
+                if angle in RELEVANT_ANGLES:
+                    output = {"quality": quality, "angle": angle, "distance": distance}
+                
+            print(json.dumps(output))
+
+    except KeyboardInterrupt:
+        print('Stopping.')
+    lidar.stop()
+    lidar.disconnect()
